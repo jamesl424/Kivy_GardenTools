bb8ccc2d48a7b7707af636675ce60ac2d838b2e1
jamesl424 <66778962+jamesl424@users.noreply.github.com>
2020-09-03 00:36:14 +0100

    V0.012
    Adjusted Carrots screen layout

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from kivy.core.window import Window
 from kivy.graphics.vertex_instructions import RoundedRectangle
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen, RiseInTransition, FallOutTransition
 
 ########################################################################################################################
@@ -27,9 +28,11 @@ from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.button import MDTextButton, MDFlatButton
 from kivymd.uix.card import MDCard
 from kivymd.uix.dialog import MDDialog
-from kivymd.uix.label import MDLabel
+from kivymd.uix.expansionpanel import MDExpansionPanel
+from kivymd.uix.label import MDLabel, MDIcon
 from kivymd.uix.list import OneLineAvatarIconListItem
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.tab import MDTabsBase
 
 ########################################################################################################################
 # Other Imports
@@ -37,6 +40,7 @@ from kivymd.uix.screen import MDScreen
 
 
 # Config Set.
+
 
 Window.size = (540, 960)
 
@@ -48,6 +52,11 @@ Window.size = (540, 960)
 ########################################################################################################################
 """Plants_Screen Class Creation"""
 ########################################################################################################################
+class Label_1(MDLabel):
+    pass
+
+class Icon_1(MDIcon):
+    pass
 
 class PlantLabels(MDLabel):
     pass
@@ -64,15 +73,19 @@ class PlantsListContent(MDBoxLayout):
 class Plants_Home(MDScreen) :
     pass
 
-
 class Apples(MDScreen):
     pass
 
 class Carrots(MDScreen) :
+    class Carrot_Wiki(FloatLayout, MDTabsBase):
+        pass
+    class Carrot_Plants(FloatLayout, MDTabsBase):
+        pass
+    class Carrot_Photos(FloatLayout, MDTabsBase):
+        pass
+
+class Tabs(MDTabsBase):
     pass
-
-
-
 
 class Screen2_2(MDScreen) :
     pass
@@ -80,7 +93,6 @@ class Screen2_2(MDScreen) :
 
 class Screen2_3(MDScreen) :
     pass
-
 
 class Screen2_4(MDScreen) :
     pass
@@ -119,6 +131,8 @@ class ItemConfirm(OneLineAvatarIconListItem) :
 
 Builder.load_file('.kv files/Plants_Screen.kv')
 Builder.load_file('.kv files/sm2_Screens.kv')
+Builder.load_file('.kv files/Apples.kv')
+Builder.load_file('.kv files/Carrots.kv')
 # Builder.load_file('.kv files/Tasks_Screen.kv')
 # Builder.load_file('.kv files/Wiki_Screen.kv')
 # Builder.load_file('.kv files/Settings_Screen.kv')
